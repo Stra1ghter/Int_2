7fb9580746ab3ca50c7a5c98fecc50500448b5fb
Simon Gensler <Simon.Gensler@skf.com>
2022-09-13 15:00:13 +0200

    Add calculator

---
+++ Calculator/Input/Input.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator.Pages;
+
+public class Input
+{
+    public string operator1 { get; set; }
+    public string operator2 { get; set; }
+    public string operand { get; set; }
+    public string result { get; set; }
+}
--- Calculator/Pages/Index.cshtml.cs
+++ Calculator/Pages/Index.cshtml.cs
@@ -7,13 +7,29 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
 
-    public void OnGet()
-    {
+    [BindProperty]
+    public Input Input { get; set; } = new Input();
 
+    public async Task<IActionResult> OnGet()
+    {
+        return this.Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        int operator1 = Convert.ToInt16(Input.operator1);
+        int operator2 = Convert.ToInt16(Input.operator2);
+        char operation = Input.operand[0];
+        if (operation == '+')
+        {
+            this.Input.result = (operator1 + operator2).ToString();
+        }
+        return this.Page();
     }
 }
